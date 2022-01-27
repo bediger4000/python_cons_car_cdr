9cb0f430b7157ba32849bdc050a6f70dddcea5a6
Bruce Ediger <bediger8@gmail.com>
2022-01-26 18:14:16 -0700

    define a few types, make a list of cons pairs

--- cons2.go
+++ cons2.go
@@ -3,18 +3,19 @@ package main
 import "fmt"
 
 type fn func(interface{}, interface{}) interface{}
+type ffn func(fn) interface{}
 
-func cons(a, b interface{}) func(func(interface{}, interface{}) interface{}) interface{} {
-	return func(f func(interface{}, interface{}) interface{}) interface{} {
+func cons(a, b interface{}) ffn {
+	return func(f fn) interface{} {
 		return f(a, b)
 	}
 }
 
-func cdr(pair func(func(interface{}, interface{}) interface{}) interface{}) interface{} {
+func cdr(pair ffn) interface{} {
 	return pair(first)
 }
 
-func car(pair func(func(interface{}, interface{}) interface{}) interface{}) interface{} {
+func car(pair ffn) interface{} {
 	return pair(second)
 }
 
@@ -28,10 +29,20 @@ func second(a interface{}, b interface{}) interface{} {
 
 func main() {
 	pair := cons(1, 2)
-	fmt.Printf("%T\n", pair)
+	fmt.Printf("pair type %T\n", pair)
 
 	y := cdr(pair)
-	fmt.Printf("%T %v\n", y, y)
+	fmt.Printf("cdr %T %v\n", y, y)
 	z := car(pair)
-	fmt.Printf("%T %v\n", z, z)
+	fmt.Printf("car %T %v\n", z, z)
+
+	var list interface{}
+	list = cons(0, cons(1, cons(2, cons(3, nil))))
+	fmt.Printf("list type %T\n", list)
+
+	var hd interface{}
+	for list != nil {
+		hd, list = cdr(list.(ffn)), car(list.(ffn))
+		fmt.Printf("head %v, rest %v\n", hd, list)
+	}
 }
